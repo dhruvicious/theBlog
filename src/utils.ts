1163c9b85217928400f2e429542edf8772f657d6
Dhruv Khatri <bedhruvicious@Dhruvs-MacBook-Air.local>
2025-10-25 12:26:22 +0530

    another type error

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
-export const range = (start, end, step = 1) => {
+export const range = (start: number, end: number, step = 1) => {
   let output = [];
   if (typeof end === "undefined") {
     end = start;
     start = 0;
   }
-  for (let i = 0; i < end; i+=step) {
+  for (let i = 0; i < end; i += step) {
     output.push(i);
   }
   return output;
